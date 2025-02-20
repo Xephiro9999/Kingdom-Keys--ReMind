5e4308bbc9fbb86f88935029d4e4c32894747015
Wehavecookies56 <qwenit56@gmail.com>
2025-02-20 12:51:21 +0000

    update KK

--- src/main/java/online/remind/remind/client/render/AutoLifeLayerRenderer.java
+++ src/main/java/online/remind/remind/client/render/AutoLifeLayerRenderer.java
@@ -39,7 +39,7 @@ public class AutoLifeLayerRenderer<T extends LivingEntity> extends RenderLayer<T
     @Override
     public void render(PoseStack matrixStackIn, MultiBufferSource bufferIn, int packedLightIn, T entitylivingbaseIn, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {        if (entitylivingbaseIn instanceof AbstractClientPlayer) {
             LivingEntityRenderer<AbstractClientPlayer, PlayerModel<AbstractClientPlayer>> renderer = (LivingEntityRenderer<AbstractClientPlayer, PlayerModel<AbstractClientPlayer>>) Minecraft.getInstance().getEntityRenderDispatcher().getRenderer((AbstractClientPlayer) entitylivingbaseIn);
-            if (!((IDisabledAnimations) renderer).isDisabled()) {
+            if (!((IDisabledAnimations) renderer).kingdom_Keys$isDisabled()) {
                 renderEntity(matrixStackIn, bufferIn, packedLightIn, entitylivingbaseIn, limbSwing, limbSwingAmount, partialTicks, ageInTicks, netHeadYaw, headPitch);
             }
         } else {
