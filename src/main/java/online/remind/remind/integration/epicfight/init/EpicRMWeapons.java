f24702260ccc97faaddee5aeb06ed5f6e7afb977
Xephiro9999 <78242238+Xephiro9999@users.noreply.github.com>
2025-12-07 13:41:08 -0500

    Trying to fix

--- src/main/java/online/remind/remind/integration/epicfight/init/EpicRMWeapons.java
+++ src/main/java/online/remind/remind/integration/epicfight/init/EpicRMWeapons.java
@@ -27,16 +27,15 @@ import yesman.epicfight.world.capabilities.item.WeaponCapability;
 import yesman.epicfight.world.capabilities.item.WeaponCategory;
 
 public class EpicRMWeapons {
-    public static final Function<Item, WeaponCapability.Builder> XEPHIRO = item -> {
-        WeaponCapability.Builder builder = WeaponCapability.builder()
+    public static final Function<Item, WeaponCapability.Builder> XEPHIRO = item ->
+        WeaponCapability.builder()
                 .category(CapabilityItem.WeaponCategories.SWORD)
                 .styleProvider(playerpatch -> playerpatch.getHoldingItemCapability(InteractionHand.OFF_HAND).getWeaponCategory() == EpicRMWeaponEnum.XEPHIRO ? CapabilityItem.Styles.TWO_HAND : CapabilityItem.Styles.ONE_HAND)
                 .hitSound(EpicFightSounds.BLADE_HIT.get())
                 .collider(ColliderPreset.LONGSWORD)
-                .weaponCombinationPredicator(entityPatch -> EpicFightCapabilities.getItemStackCapability(entityPatch.getOriginal().getOffhandItem()).getWeaponCategory() == EpicRMWeaponEnum.XEPHIRO)
+                .weaponCombinationPredicator(entityPatch -> EpicFightCapabilities.getItemStackCapability(entityPatch.getOriginal().getOffhandItem()).getWeaponCategory() == CapabilityItem.WeaponCategories.SWORD)
                 .newStyleCombo(CapabilityItem.Styles.ONE_HAND, Animations.GREATSWORD_AUTO1, Animations.GREATSWORD_AUTO2, Animations.TACHI_AUTO1, KKAnimations.SORA_FINISHER1, KKAnimations.SORA_AUTO3, Animations.GREATSWORD_AIR_SLASH)
                 .newStyleCombo(CapabilityItem.Styles.TWO_HAND, Animations.SWORD_DUAL_AUTO1, KKAnimations.VALOR_AUTO1, KKAnimations.VALOR_AUTO2, KKAnimations.VALOR_AUTO3, Animations.SWORD_DUAL_DASH, Animations.SWORD_DUAL_AIR_SLASH)
-                .newStyleCombo(CapabilityItem.Styles.MOUNT, Animations.SWORD_MOUNT_ATTACK).innateSkill(CapabilityItem.Styles.ONE_HAND, itemstack -> Skill.skillOrNull(EpicFightSkills.EVISCERATE)).innateSkill(CapabilityItem.Styles.TWO_HAND, itemstack -> Skill.skillOrNull(EpicFightSkills.BLADE_RUSH))
                 .livingMotionModifier(CapabilityItem.Styles.ONE_HAND, LivingMotions.IDLE, Animations.BIPED_HOLD_LONGSWORD)
                 .livingMotionModifier(CapabilityItem.Styles.TWO_HAND, LivingMotions.IDLE, Animations.BIPED_HOLD_DUAL_WEAPON)
                 .livingMotionModifier(CapabilityItem.Styles.ONE_HAND, LivingMotions.WALK, Animations.BIPED_WALK_SPEAR)
@@ -44,19 +43,21 @@ public class EpicRMWeapons {
                 .livingMotionModifier(CapabilityItem.Styles.ONE_HAND, LivingMotions.RUN, Animations.BIPED_RUN_SPEAR)
                 .livingMotionModifier(CapabilityItem.Styles.TWO_HAND, LivingMotions.RUN, Animations.BIPED_RUN_DUAL)
                 .livingMotionModifier(CapabilityItem.Styles.ONE_HAND, LivingMotions.BLOCK, Animations.LONGSWORD_GUARD)
-                .livingMotionModifier(CapabilityItem.Styles.TWO_HAND, LivingMotions.BLOCK, Animations.SWORD_DUAL_GUARD);
+                .livingMotionModifier(CapabilityItem.Styles.TWO_HAND, LivingMotions.BLOCK, Animations.SWORD_DUAL_GUARD)
+                .passiveSkill(KKSkills.comboExtender.get());
 
 
         //TODO: Add Styles for FF Characters as either base styles or drive forms.
 
-        return builder;
-    };
+
+
 
     private EpicRMWeapons() {
     }
 
     public static void register(WeaponCapabilityPresetRegistryEvent event) {
-        event.getTypeEntry().put(ResourceLocation.fromNamespaceAndPath(KingdomKeysReMind.MODID,EpicRMWeaponEnum.XEPHIRO.toString().toLowerCase()), (Function<Item, ? extends CapabilityItem.Builder<?>>) XEPHIRO);
+        event.getTypeEntry().put(ResourceLocation.fromNamespaceAndPath(KingdomKeysReMind.MODID,EpicRMWeaponEnum.XEPHIRO.toString().toLowerCase()), XEPHIRO);
+
     }
 
     public enum EpicRMWeaponEnum implements WeaponCategory {
