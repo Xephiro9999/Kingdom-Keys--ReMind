3b676c452d79eadf4f0bd6d758e54c6287fbaf59
WillabyNeko <37820156+WillabyNeko@users.noreply.github.com>
2025-12-20 13:34:38 -0500

    Stuff

--- src/main/java/online/remind/remind/driveform/DriveFormTwilight.java
+++ src/main/java/online/remind/remind/driveform/DriveFormTwilight.java
@@ -1,24 +1,59 @@
 package online.remind.remind.driveform;
 
+import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.living.LivingDeathEvent;
+import online.kingdomkeys.kingdomkeys.config.ModConfigs;
 import online.kingdomkeys.kingdomkeys.data.PlayerData;
 import online.kingdomkeys.kingdomkeys.driveform.DriveForm;
 import online.kingdomkeys.kingdomkeys.item.ModItems;
+import online.kingdomkeys.kingdomkeys.network.PacketHandler;
+import online.kingdomkeys.kingdomkeys.network.stc.SCSyncPlayerData;
 import online.remind.remind.KingdomKeysReMind;
+//import online.remind.remind.config.ModConfigs;
 import online.remind.remind.lib.StringsRM;
 
+@EventBusSubscriber(modid = KingdomKeysReMind.MODID)
 public class DriveFormTwilight extends DriveForm {
-    public DriveFormTwilight(ResourceLocation registeryName, int order, ResourceLocation skinRL, boolean hasKeychain, boolean baseGrowthAbilities) {
-        super(registeryName, order, hasKeychain, baseGrowthAbilities);
-        this.color = new float[] {0.25F,0.25F,0.25F};
-        this.skinRL = skinRL;
-    }
+	public DriveFormTwilight(ResourceLocation registryName, int order, ResourceLocation skinRL, boolean hasKeychain, boolean baseGrowthAbilities) {
+		super(registryName, order, hasKeychain, baseGrowthAbilities);
+		this.color = new float[] {0.25F,0.25F,0.25F};
+		this.skinRL = skinRL;
+	}
 
-    // Twilight Form EXP Gain -- If decided to let the form level
+	private static final TagKey<EntityType<?>> BOSS_TAG = TagKey.create(
+			Registries.ENTITY_TYPE,
+			ResourceLocation.fromNamespaceAndPath("c", "bosses")
+	);
 
-    @Override
-    public boolean isSlotVisible(Player player) {
+	// Twilight Form EXP Gain -- If decided to let the form level
+	@SubscribeEvent
+	public static void getTwilightFormXP(LivingDeathEvent event) {
+		if (!event.getEntity().level().isClientSide && event.getEntity() instanceof LivingEntity killed) {
+			// Logic to detect if the mob is a boss (Tag check)
+			if (killed.getType().is(BOSS_TAG)) {
+				if (event.getSource().getEntity() instanceof Player player) {
+					PlayerData playerData = PlayerData.get(player);
+					String twilightID = KingdomKeysReMind.MODID + ":" + StringsRM.twilight;
+					if (playerData != null && playerData.getActiveDriveForm().equals(twilightID)) {
+						double mult = Double.parseDouble(ModConfigs.SERVER.driveFormXPMultiplier.get().get(1).split(",")[1]);
+						playerData.setDriveFormExp(player, playerData.getActiveDriveForm(), (int) (playerData.getDriveFormExp(playerData.getActiveDriveForm()) + (1 * mult)));
+						PacketHandler.sendTo(new SCSyncPlayerData(player), (ServerPlayer) player);
+					}
+				}
+			}
+		}
+	}
+
+	@Override
+	public boolean isSlotVisible(Player player) {
 		PlayerData playerData = PlayerData.get(player);
 		if (playerData != null) {
 			if (playerData.isAbilityEquipped(StringsRM.roadToDawn)) {
