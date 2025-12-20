d6e5510614ff57e87b4bcbb5c36fee3c89c1ba54
Xephiro9999 <78242238+Xephiro9999@users.noreply.github.com>
2025-12-20 11:16:05 -0500

    Update EntityEventsRM.java

--- src/main/java/online/remind/remind/handler/EntityEventsRM.java
+++ src/main/java/online/remind/remind/handler/EntityEventsRM.java
@@ -378,6 +378,7 @@ public class EntityEventsRM {
 					updateDriveAbilities(player, StringsRM.darkPower, KingdomKeysReMind.MODID + ":" + StringsRM.darkForm);
 					updateDriveAbilities(player, StringsRM.rageAwakened, KingdomKeysReMind.MODID + ":" + StringsRM.rageForm);
 					updateDriveAbilities(player, StringsRM.wayToLight, KingdomKeysReMind.MODID + ":" + StringsRM.lightForm);
+					updateDriveAbilities(player, StringsRM.roadToDawn, KingdomKeysReMind.MODID+":"+ StringsRM.twilight);
 					updateDriveAbilities(player, StringsRM.Regen, KingdomKeysReMind.MODID + ":" + StringsRM.regenForm);
 
 					// Light/Darkness Within
